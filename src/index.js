a65499b6356bd7444c66a3c09cd3e930875c5472
Lucas Bittencourt <lucasnaja0@gmail.com>
2020-01-20 18:10:28 -0300

    version 1.0.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { beautifyNotFollowers } = require('./utils/beautifyNotFollowers');
 const { whoDoesNotFollowMe } = require('./utils/whoDoesNotFollowMe');
 
+beautifyNotFollowers({ username: 'lucasnaja' });
+
 module.exports = { beautifyNotFollowers, whoDoesNotFollowMe };
