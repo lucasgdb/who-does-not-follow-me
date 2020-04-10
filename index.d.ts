4da1e02eb7331e9d192d35688a1db4460fd8531d
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-04-10 12:52:38 -0300

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,20 @@
-declare function whoDoesNotFollowMe({
-	username,
-	client_id,
-	client_secret,
-}?: {
-	username?: string | undefined;
-	client_id?: string | undefined;
-	client_secret?: string | undefined;
-}): Promise<
-	| {
-			thesePeopleDoNotFollowMe: undefined;
-			message: any;
-	  }
-	| {
-			thesePeopleDoNotFollowMe: string[];
-			message?: undefined;
-	  }
->;
+declare module 'who-does-not-follow-me' {
+	export default function whoDoesNotFollowMe({
+		username,
+		client_id,
+		client_secret,
+	}?: {
+		username?: string | undefined;
+		client_id?: string | undefined;
+		client_secret?: string | undefined;
+	}): Promise<
+		| {
+				thesePeopleDoNotFollowMe: undefined;
+				message: any;
+		  }
+		| {
+				thesePeopleDoNotFollowMe: string[];
+				message?: undefined;
+		  }
+	>;
+}
